42291bee8e5e6c994f9b1ff875de71f18fa4614b
Xotic750 <xotic750@gmail.com>
2017-03-10 21:42:13 +0100

    Use yarn, update dependencies, add latest node versions, cleanup

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 <a name="module_assert-is-object-x"></a>
+
 ## assert-is-object-x
 <a href="https://travis-ci.org/Xotic750/assert-is-object-x"
 title="Travis status">
@@ -41,11 +42,12 @@ es5-shim.js to be able to work properly.
 `es6.shim.js` provides compatibility shims so that legacy JavaScript engines
 behave as closely as possible to ECMAScript 6 (Harmony).
 
-**Version**: 1.0.14  
-**Author:** Xotic750 <Xotic750@gmail.com>  
+**Version**: 1.1.0  
+**Author**: Xotic750 <Xotic750@gmail.com>  
 **License**: [MIT](&lt;https://opensource.org/licenses/MIT&gt;)  
 **Copyright**: Xotic750  
 <a name="exp_module_assert-is-object-x--module.exports"></a>
+
 ### `module.exports(value)` ⇒ <code>\*</code> ⏏
 Tests `value` to see if it is an object, throws a `TypeError` if it is
 not. Otherwise returns the `value`.
--- tests/spec/test.js
+++ tests/spec/test.js
@@ -1,21 +1,24 @@
-/*jslint maxlen:80, es6:true, white:true */
+/* jslint maxlen:80, es6:true, white:true */
 
-/*jshint bitwise:true, camelcase:true, curly:true, eqeqeq:true, forin:true,
-  freeze:true, futurehostile:true, latedef:true, newcap:true, nocomma:true,
-  nonbsp:true, singleGroups:true, strict:true, undef:true, unused:true,
-  es3:false, esnext:true, plusplus:true, maxparams:1, maxdepth:2,
-  maxstatements:11, maxcomplexity:4 */
+/* jshint bitwise:true, camelcase:true, curly:true, eqeqeq:true, forin:true,
+   freeze:true, futurehostile:true, latedef:true, newcap:true, nocomma:true,
+   nonbsp:true, singleGroups:true, strict:true, undef:true, unused:true,
+   es3:false, esnext:true, plusplus:true, maxparams:1, maxdepth:2,
+   maxstatements:12, maxcomplexity:4 */
 
-/*global JSON:true, expect, module, jasmine, require, describe, xit, it,
+/* eslint strict: 1, max-lines: 1, symbol-description: 1, max-nested-callbacks: 1,
+   max-statements: 1 */
+
+/* global JSON:true, expect, module, jasmine, require, describe, xit, it,
   returnExports */
 
-(function () {
+;(function () { // eslint-disable-line no-extra-semi
+
   'use strict';
 
-  var hasSymbolSupport = typeof Symbol === 'function' &&
-      typeof Symbol() === 'symbol',
-    ifSymbolSupportIt = hasSymbolSupport ? it : xit,
-    assertIsObject;
+  var hasSymbolSupport = typeof Symbol === 'function' && typeof Symbol() === 'symbol';
+  var ifSymbolSupportIt = hasSymbolSupport ? it : xit;
+  var assertIsObject;
   if (typeof module === 'object' && module.exports) {
     require('es5-shim');
     require('es5-shim/es5-sham');
@@ -24,6 +27,13 @@
     }
     require('json3').runInContext(null, JSON);
     require('es6-shim');
+    var es7 = require('es7-shim');
+    Object.keys(es7).forEach(function (key) {
+      var obj = es7[key];
+      if (typeof obj.shim === 'function') {
+        obj.shim();
+      }
+    });
     assertIsObject = require('../../index.js');
   } else {
     assertIsObject = returnExports;
@@ -31,7 +41,7 @@
 
   describe('assertIsObject', function () {
     it('primitives should throw a TypeError', function () {
-      function block(value) {
+      var block = function (value) {
         try {
           assertIsObject(value);
           return false;
@@ -40,17 +50,17 @@
           expect(e.message).toBe(String(value) + ' is not an object');
         }
         return true;
-      }
-      var values = [undefined, null, 1, true, ''],
-        expected = values.map(function () {
-          return true;
-        }),
-        actual = values.map(block);
+      };
+      var values = [undefined, null, 1, true, ''];
+      var expected = values.map(function () {
+        return true;
+      });
+      var actual = values.map(block);
       expect(actual).toEqual(expected);
     });
 
     ifSymbolSupportIt('Symbol literals should throw a TypeError', function () {
-      function block(value) {
+      var block = function (value) {
         try {
           assertIsObject(value);
           return false;
@@ -59,13 +69,13 @@
           expect(e.message).toBe('Symbol(mySymbol) is not an object');
         }
         return true;
-      }
-      var sym = Symbol('mySymbol'),
-        values = [sym],
-        expected = values.map(function () {
-          return true;
-        }),
-        actual = values.map(block);
+      };
+      var sym = Symbol('mySymbol');
+      var values = [sym];
+      var expected = values.map(function () {
+        return true;
+      });
+      var actual = values.map(block);
       expect(actual).toEqual(expected);
     });
 
@@ -75,26 +85,26 @@
     });
 
     it('should return the object', function () {
-      function block(value) {
+      var block = function (value) {
         try {
           return assertIsObject(value);
         } catch (ignore) {}
         return false;
-      }
+      };
       var values = [
-          function () {},
-          Array,
-          block,
-          assertIsObject,
-          [],
-          {},
-          /r/,
-          new Date()
-        ],
-        expected = values.map(function (x) {
-          return x;
-        }),
-        actual = values.map(block);
+        function () {},
+        Array,
+        block,
+        assertIsObject,
+        [],
+        {},
+        /r/,
+        new Date()
+      ];
+      var expected = values.map(function (x) {
+        return x;
+      });
+      var actual = values.map(block);
       expect(actual).toEqual(expected);
     });
   });
