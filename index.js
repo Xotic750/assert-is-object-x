935c3fd743b359a13bf9220193a689f818b06bf4
Xotic750 <xotic750@gmail.com>
2017-07-11 13:57:05 +0200

    Update dependencies

--- index.js
+++ index.js
@@ -1,54 +1,24 @@
 /**
- * @file
- * <a href="https://travis-ci.org/Xotic750/assert-is-object-x"
- * title="Travis status">
- * <img
- * src="https://travis-ci.org/Xotic750/assert-is-object-x.svg?branch=master"
- * alt="Travis status" height="18">
- * </a>
- * <a href="https://david-dm.org/Xotic750/assert-is-object-x"
- * title="Dependency status">
- * <img src="https://david-dm.org/Xotic750/assert-is-object-x.svg"
- * alt="Dependency status" height="18"/>
- * </a>
- * <a
- * href="https://david-dm.org/Xotic750/assert-is-object-x#info=devDependencies"
- * title="devDependency status">
- * <img src="https://david-dm.org/Xotic750/assert-is-object-x/dev-status.svg"
- * alt="devDependency status" height="18"/>
- * </a>
- * <a href="https://badge.fury.io/js/assert-is-object-x" title="npm version">
- * <img src="https://badge.fury.io/js/assert-is-object-x.svg"
- * alt="npm version" height="18">
- * </a>
- *
- * If IsObject(value) is false, throw a TypeError exception.
- *
- * @version 1.2.0
+ * @file If IsObject(value) is false, throw a TypeError exception.
+ * @version 1.3.0
  * @author Xotic750 <Xotic750@gmail.com>
  * @copyright  Xotic750
  * @license {@link <https://opensource.org/licenses/MIT> MIT}
  * @module assert-is-object-x
  */
 
-/* eslint strict: 1 */
+'use strict';
 
-/* global require, module */
+var safeToString = require('safe-to-string-x');
+var isPrimitive = require('is-primitive');
 
-;(function () { // eslint-disable-line no-extra-semi
-
-  'use strict';
-
-  var safeToString = require('safe-to-string-x');
-  var isPrimitive = require('is-primitive');
-
-  /**
+/**
    * Tests `value` to see if it is an object, throws a `TypeError` if it is
    * not. Otherwise returns the `value`.
    *
-   * @param {*} value The argument to be tested.
+   * @param {*} value - The argument to be tested.
    * @throws {TypeError} Throws if `value` is not an object.
-   * @return {*} Returns `value` if it is an object.
+   * @returns {*} Returns `value` if it is an object.
    * @example
    * var assertIsObject = require('assert-is-object-x');
    * var primitive = true;
@@ -63,10 +33,9 @@
    * assertIsObject(object); // Returns object.
    * assertIsObject(fn); // Returns fn.
    */
-  module.exports = function assertIsObject(value) {
-    if (isPrimitive(value)) {
-      throw new TypeError(safeToString(value) + ' is not an object');
-    }
-    return value;
-  };
-}());
+module.exports = function assertIsObject(value) {
+  if (isPrimitive(value)) {
+    throw new TypeError(safeToString(value) + ' is not an object');
+  }
+  return value;
+};
--- tests/spec/test.js
+++ tests/spec/test.js
@@ -1,111 +1,100 @@
-/* jslint maxlen:80, es6:true, white:true */
+'use strict';
 
-/* jshint bitwise:true, camelcase:true, curly:true, eqeqeq:true, forin:true,
-   freeze:true, futurehostile:true, latedef:true, newcap:true, nocomma:true,
-   nonbsp:true, singleGroups:true, strict:true, undef:true, unused:true,
-   es3:false, esnext:true, plusplus:true, maxparams:1, maxdepth:2,
-   maxstatements:12, maxcomplexity:4 */
-
-/* eslint strict: 1, max-lines: 1, symbol-description: 1, max-nested-callbacks: 1,
-   max-statements: 1 */
-
-/* global JSON:true, expect, module, jasmine, require, describe, xit, it,
-  returnExports */
-
-;(function () { // eslint-disable-line no-extra-semi
-
-  'use strict';
-
-  var hasSymbolSupport = typeof Symbol === 'function' && typeof Symbol() === 'symbol';
-  var ifSymbolSupportIt = hasSymbolSupport ? it : xit;
-  var assertIsObject;
-  if (typeof module === 'object' && module.exports) {
-    require('es5-shim');
-    require('es5-shim/es5-sham');
-    if (typeof JSON === 'undefined') {
-      JSON = {};
-    }
-    require('json3').runInContext(null, JSON);
-    require('es6-shim');
-    var es7 = require('es7-shim');
-    Object.keys(es7).forEach(function (key) {
-      var obj = es7[key];
-      if (typeof obj.shim === 'function') {
-        obj.shim();
-      }
-    });
-    assertIsObject = require('../../index.js');
-  } else {
-    assertIsObject = returnExports;
+var hasSymbolSupport = typeof Symbol === 'function' && typeof Symbol('') === 'symbol';
+var ifSymbolSupportIt = hasSymbolSupport ? it : xit;
+var assertIsObject;
+if (typeof module === 'object' && module.exports) {
+  require('es5-shim');
+  require('es5-shim/es5-sham');
+  if (typeof JSON === 'undefined') {
+    JSON = {};
   }
-
-  describe('assertIsObject', function () {
-    it('primitives should throw a TypeError', function () {
-      var block = function (value) {
-        try {
-          assertIsObject(value);
-          return false;
-        } catch (e) {
-          expect(e).toEqual(jasmine.any(TypeError));
-          expect(e.message).toBe(String(value) + ' is not an object');
-        }
-        return true;
-      };
-      var values = [undefined, null, 1, true, ''];
-      var expected = values.map(function () {
-        return true;
-      });
-      var actual = values.map(block);
-      expect(actual).toEqual(expected);
-    });
-
-    ifSymbolSupportIt('Symbol literals should throw a TypeError', function () {
-      var block = function (value) {
-        try {
-          assertIsObject(value);
-          return false;
-        } catch (e) {
-          expect(e).toEqual(jasmine.any(TypeError));
-          expect(e.message).toBe('Symbol(mySymbol) is not an object');
-        }
-        return true;
-      };
-      var sym = Symbol('mySymbol');
-      var values = [sym];
-      var expected = values.map(function () {
-        return true;
-      });
-      var actual = values.map(block);
-      expect(actual).toEqual(expected);
-    });
-
-    ifSymbolSupportIt('Symbol objects should return the object', function () {
-      var sym = Object(Symbol('mySymbol'));
-      expect(assertIsObject(sym)).toEqual(sym);
-    });
-
-    it('should return the object', function () {
-      var block = function (value) {
-        try {
-          return assertIsObject(value);
-        } catch (ignore) {}
-        return false;
-      };
-      var values = [
-        function () {},
-        Array,
-        block,
-        assertIsObject,
-        [],
-        {},
-        /r/,
-        new Date()
-      ];
-      var expected = values.map(function (x) {
-        return x;
-      });
-      var actual = values.map(block);
-      expect(actual).toEqual(expected);
-    });
+  require('json3').runInContext(null, JSON);
+  require('es6-shim');
+  var es7 = require('es7-shim');
+  Object.keys(es7).forEach(function (key) {
+    var obj = es7[key];
+    if (typeof obj.shim === 'function') {
+      obj.shim();
+    }
   });
-}());
+  assertIsObject = require('../../index.js');
+} else {
+  assertIsObject = returnExports;
+}
+
+describe('assertIsObject', function () {
+  it('primitives should throw a TypeError', function () {
+    var block = function (value) {
+      try {
+        assertIsObject(value);
+        return false;
+      } catch (e) {
+        expect(e).toEqual(jasmine.any(TypeError));
+        expect(e.message).toBe(String(value) + ' is not an object');
+      }
+      return true;
+    };
+    var values = [
+      undefined,
+      null,
+      1,
+      true,
+      ''
+    ];
+    var expected = values.map(function () {
+      return true;
+    });
+    var actual = values.map(block);
+    expect(actual).toEqual(expected);
+  });
+
+  ifSymbolSupportIt('Symbol literals should throw a TypeError', function () {
+    var block = function (value) {
+      try {
+        assertIsObject(value);
+        return false;
+      } catch (e) {
+        expect(e).toEqual(jasmine.any(TypeError));
+        expect(e.message).toBe('Symbol(mySymbol) is not an object');
+      }
+      return true;
+    };
+    var sym = Symbol('mySymbol');
+    var values = [sym];
+    var expected = values.map(function () {
+      return true;
+    });
+    var actual = values.map(block);
+    expect(actual).toEqual(expected);
+  });
+
+  ifSymbolSupportIt('Symbol objects should return the object', function () {
+    var sym = Object(Symbol('mySymbol'));
+    expect(assertIsObject(sym)).toEqual(sym);
+  });
+
+  it('should return the object', function () {
+    var block = function (value) {
+      try {
+        return assertIsObject(value);
+      } catch (ignore) {}
+      return false;
+    };
+    var values = [
+      function () {},
+      Array,
+      block,
+      assertIsObject,
+      [],
+      {},
+      /r/,
+      new Date()
+    ];
+    var expected = values.map(function (x) {
+      return x;
+    });
+    var actual = values.map(block);
+    expect(actual).toEqual(expected);
+  });
+});
