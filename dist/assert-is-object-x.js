08a2531c56d3a8e8679a7794c1bb05b160beaaf7
Graham Fairweather <xotic750@gmail.com>
2019-07-18 20:46:29 +0200

    :bookmark: v3.0.7
    Auto build

--- dist/assert-is-object-x.js
+++ dist/assert-is-object-x.js
@@ -2,13 +2,13 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2015-2017",
-  "date": "2019-07-17T23:50:51.692Z",
+  "date": "2019-07-18T18:46:23.360Z",
   "describe": "",
   "description": "If IsObject(value) is false, throw a TypeError exception.",
   "file": "assert-is-object-x.js",
   "hash": "f39d825d7707c00bfdfa",
   "license": "MIT",
-  "version": "3.0.6"
+  "version": "3.0.7"
 }
 */
 (function webpackUniversalModuleDefinition(root, factory) {
