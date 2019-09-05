3be5aca669d3bfd25c0e6ed24776a430b8bbb797
Graham Fairweather <xotic750@gmail.com>
2019-09-05 21:26:36 +0200

    Maintenance

--- dist/assert-is-object-x.js
+++ dist/assert-is-object-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2015-2017",
-  "date": "2019-09-04T23:18:40.879Z",
+  "date": "2019-09-05T19:26:30.756Z",
   "describe": "",
   "description": "If IsObject(value) is false, throw a TypeError exception.",
   "file": "assert-is-object-x.js",
